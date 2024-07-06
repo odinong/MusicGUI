a4716dd54dcc6cb8533b94d14d117ddbec0c9c48
Kalz <geeopp21@gmail.com>
2024-07-06 03:45:18 -0400

    Fixed GUI Breaking In Some Games, Also Fixed Crashes In Gorilla Tag.

--- MainMenu/Plugin.cs
+++ MainMenu/Plugin.cs
@@ -31,13 +31,7 @@ namespace MusicPlayer
         private int currentPage = 0;
         private int playlistPage = 0;
 
-        private GUILayoutOption[] buttonLayoutOptions = { GUILayout.Height(30) };
-        private GUILayoutOption[] sliderLayoutOptions = { GUILayout.Width(300) };
-        private GUILayoutOption[] paginationLayoutOptions = { GUILayout.ExpandWidth(true) };
-        private GUIStyle toggleStyle;
-        private GUIStyle labelStyle;
-        private GUIStyle boxStyle;
-        private GUIStyle buttonStyle;
+
         private bool darkMode = false;
         private Color col;
         private Color col2;
@@ -113,85 +107,17 @@ namespace MusicPlayer
 
         void OnGUI()
         {
-            // GUI Style Setup
-            if (darkMode == false)
-            {
-                col = Color.white;
-                col2 = Color.white;
-                col3 = Color.white;
-            }
-            if (darkMode)
-            {
-                coll4 = Color.black;
-                coll5 = Color.black;
-                col6 = Color.black;
-            }
-            boxStyle = new GUIStyle(GUI.skin.box);
-            boxStyle.normal.textColor = Color.white;
-            boxStyle.normal.background = MakeTex(2, 2, col);
-            boxStyle.fontSize = 12;
-            boxStyle.padding = new RectOffset(10, 10, 10, 10);
-            if (darkMode == false)
-            {
-                buttonStyle = new GUIStyle(GUI.skin.button);
-                buttonStyle.normal.textColor = Color.black;
-                buttonStyle.normal.background = MakeTex(2, 2, col2);
-                buttonStyle.hover.background = MakeTex(2, 2, col3);
-                buttonStyle.fontSize = 12;
-                buttonStyle.padding = new RectOffset(10, 10, 5, 5);
-                labelStyle = new GUIStyle(GUI.skin.label);
-                labelStyle.normal.textColor = Color.black;
-                labelStyle.fontSize = 12;
-            }
-            if (darkMode == true)
-            {
-                buttonStyle = new GUIStyle(GUI.skin.button);
-                buttonStyle.normal.textColor = Color.white;
-                buttonStyle.normal.background = MakeTex(2, 2, coll4);
-                buttonStyle.hover.background = MakeTex(2, 2, coll5);
-                buttonStyle.fontSize = 12;
-                buttonStyle.padding = new RectOffset(10, 10, 5, 5);
-                labelStyle = new GUIStyle(GUI.skin.label);
-                labelStyle.normal.textColor = Color.white;
-                labelStyle.fontSize = 12;
-            }
-            Color col4 = new Color(0.2f, 0.2f, 0.2f, 1f);
-            Color col5 = new Color(0.2f, 0.2f, 0.6f, 1f);
-            Color white2 = Color.white;
-            toggleStyle = new GUIStyle(GUI.skin.toggle);
-            toggleStyle.normal.textColor = white2;
-            toggleStyle.onNormal.textColor = white2;
-            toggleStyle.hover.textColor = white2;
-            toggleStyle.onHover.textColor = white2;
-            toggleStyle.focused.textColor = white2;
-            toggleStyle.onFocused.textColor = white2;
-            toggleStyle.active.textColor = white2;
-            toggleStyle.onActive.textColor = white2;
-            toggleStyle.normal.background = MakeTex(2, 2, col4);
-            toggleStyle.onNormal.background = MakeTex(2, 2, col5);
-            toggleStyle.hover.background = MakeTex(2, 2, col4);
-            toggleStyle.onHover.background = MakeTex(2, 2, col5);
-            toggleStyle.focused.background = MakeTex(2, 2, col4);
-            toggleStyle.onFocused.background = MakeTex(2, 2, col5);
-            toggleStyle.active.background = MakeTex(2, 2, col4);
-            toggleStyle.onActive.background = MakeTex(2, 2, col5);
-            toggleStyle.border = new RectOffset(1, 1, 1, 1);
-            toggleStyle.margin = new RectOffset(4, 4, 4, 4);
-            toggleStyle.padding = new RectOffset(4, 4, 4, 4);
-            toggleStyle.fontSize = 12;
-            toggleStyle.alignment = TextAnchor.MiddleCenter;
-
             GUILayout.Label("Audio Player By Odin", GUILayout.Height(30));
-            if (GUILayout.Button(showGUI ? "Hide GUI" : "Show GUI", buttonStyle, GUILayout.Height(30)))
+            if (GUILayout.Button(showGUI ? "Hide GUI" : "Show GUI", GUILayout.Height(30)))
             {
                 showGUI = !showGUI;
             }
-            if (GUILayout.Button("Rescan Folder", buttonStyle))
+            if (GUILayout.Button("Rescan Folder"))
             {
                 audioFiles.Clear();
                 ScanFolder();
             }
-            if (GUILayout.Button("Dark Mode: " + YesNo(darkMode), buttonStyle))
+            if (GUILayout.Button("Dark Mode: " + YesNo(darkMode)))
             {
                 darkMode = !darkMode;
             }
@@ -200,16 +126,15 @@ namespace MusicPlayer
             {
                 return;
             }
-
             GUILayout.Space(10);
 
             // Tab buttons
             GUILayout.BeginHorizontal();
-            if (GUILayout.Button("Music", buttonStyle))
+            if (GUILayout.Button("Music"))
             {
                 currentTab = "Music";
             }
-            if (GUILayout.Button("Playlists", buttonStyle))
+            if (GUILayout.Button("Playlists"))
             {
                 currentTab = "Playlists";
             }
@@ -226,64 +151,54 @@ namespace MusicPlayer
             }
 
             GUILayout.Label("Volume");
-            volume = GUILayout.HorizontalSlider(volume, 0.0f, 1.0f, sliderLayoutOptions);
+            volume = GUILayout.HorizontalSlider(volume, 0.0f, 1.0f);
             audioSource.volume = volume;
 
-            GUILayout.Label("Speed");
-            speed = GUILayout.HorizontalSlider(speed, 0.5f, 2.0f, sliderLayoutOptions);
-            audioSource.pitch = speed;
-
-            GUILayout.Label("Pitch");
-            pitch = GUILayout.HorizontalSlider(pitch, 0.5f, 2.0f, sliderLayoutOptions);
+            GUILayout.Label("Speed + Pitch");
+            pitch = GUILayout.HorizontalSlider(pitch, 0.5f, 2.0f);
             audioSource.pitch = pitch;
 
             GUILayout.Space(10);
 
             GUILayout.Label("Reverb Level");
-            reverbLevel = GUILayout.HorizontalSlider(reverbLevel, -10000.0f, 1000.0f, sliderLayoutOptions);
+            reverbLevel = GUILayout.HorizontalSlider(reverbLevel, -10000.0f, 1000.0f);
             reverbFilter.reverbLevel = reverbLevel;
 
             GUILayout.Space(10);
 
-            loopAudio = GUILayout.Toggle(loopAudio, "Loop Audio", toggleStyle);
+            loopAudio = GUILayout.Toggle(loopAudio, "Loop Audio");
 
             GUILayout.Space(10);
 
             if (audioSource.isPlaying)
             {
-                if (GUILayout.Button("Pause", buttonStyle, GUILayout.Height(30)))
+                if (GUILayout.Button("Pause", GUILayout.Height(30)))
                 {
                     audioSource.Pause();
                 }
-                if (GUILayout.Button("Stop", buttonStyle, GUILayout.Height(30)))
+                if (GUILayout.Button("Stop", GUILayout.Height(30)))
                 {
                     audioSource.Stop();
                 }
-                if (GUILayout.Button("Slowed + Reverb Audio", buttonStyle, GUILayout.Height(30)))
+                if (GUILayout.Button("Slowed + Reverb Audio", GUILayout.Height(30)))
                 {
                     pitch = 0.8902f;
                     reverbLevel = 0f;
                 }
-                if (GUILayout.Button("Sped Up", buttonStyle, GUILayout.Height(30)))
+                if (GUILayout.Button("Sped Up", GUILayout.Height(30)))
                 {
                     pitch = 1.20f;
                     reverbLevel = -10000f;
                 }
-                if (GUILayout.Button("Normal", buttonStyle, GUILayout.Height(30)))
+                if (GUILayout.Button("Normal", GUILayout.Height(30)))
                 {
                     pitch = 1f;
                     reverbLevel = -10000f;
                 }
-                if (GUILayout.Button("Sped Up Config", buttonStyle, GUILayout.Height(30)))
-                {
-                    pitch = 1.20f;
-                    reverbLevel = -10000f;
-                    volume = 0.809f;
-                }
             }
             else if (audioSource.time > 0)
             {
-                if (GUILayout.Button("Resume", buttonStyle, GUILayout.Height(30)))
+                if (GUILayout.Button("Resume", GUILayout.Height(30)))
                 {
                     audioSource.Play();
                 }
@@ -298,27 +213,27 @@ namespace MusicPlayer
             for (int i = startIndex; i < endIndex; i++)
             {
                 GUILayout.BeginHorizontal();
-                if (GUILayout.Button(Path.GetFileName(audioFiles[i]), buttonStyle, buttonLayoutOptions))
+                if (GUILayout.Button(Path.GetFileName(audioFiles[i])))
                 {
                     PlayAudio(audioFiles[i]);
                 }
 
                 if (!string.IsNullOrEmpty(selectedPlaylist))
                 {
-                    if (GUILayout.Button("Add to Playlist", buttonStyle, buttonLayoutOptions))
+                    if (GUILayout.Button("Add to Playlist"))
                     {
                         AddToPlaylist(selectedPlaylist, audioFiles[i]);
                     }
                 }
                 GUILayout.EndHorizontal();
             }
-            GUILayout.BeginHorizontal(paginationLayoutOptions);
+            GUILayout.BeginHorizontal();
             GUILayout.FlexibleSpace();
-            if (currentPage > 0 && GUILayout.Button("Previous Page", buttonStyle, buttonLayoutOptions))
+            if (currentPage > 0 && GUILayout.Button("Previous Page"))
             {
                 currentPage--;
             }
-            if (endIndex < audioFiles.Count && GUILayout.Button("Next Page", buttonStyle, buttonLayoutOptions))
+            if (endIndex < audioFiles.Count && GUILayout.Button("Next Page"))
             {
                 currentPage++;
             }
@@ -333,18 +248,18 @@ namespace MusicPlayer
             foreach (var playlist in playlists)
             {
                 GUILayout.BeginHorizontal();
-                if (GUILayout.Button(playlist.Key, buttonStyle, buttonLayoutOptions))
+                if (GUILayout.Button(playlist.Key))
                 {
                     selectedPlaylist = playlist.Key;
-                    playlistPage = 0; // Reset to first page when a new playlist is selected
+                    playlistPage = 0;
                 }
-                if (GUILayout.Button("Delete", buttonStyle, buttonLayoutOptions))
+                if (GUILayout.Button("Delete"))
                 {
                     playlists.Remove(playlist.Key);
                     SavePlaylists();
                     selectedPlaylist = "";
                 }
-                if (GUILayout.Button("Play", buttonStyle, buttonLayoutOptions))
+                if (GUILayout.Button("Play"))
                 {
                     PlayPlaylist(playlist.Key);
                 }
@@ -355,7 +270,7 @@ namespace MusicPlayer
 
             GUILayout.Label("Create New Playlist");
             newPlaylistName = GUILayout.TextField(newPlaylistName, 25);
-            if (GUILayout.Button("Create Playlist", buttonStyle))
+            if (GUILayout.Button("Create Playlist"))
             {
                 if (!string.IsNullOrEmpty(newPlaylistName) && !playlists.ContainsKey(newPlaylistName))
                 {
@@ -378,8 +293,8 @@ namespace MusicPlayer
                 for (int i = startIndex; i < endIndex; i++)
                 {
                     GUILayout.BeginHorizontal();
-                    GUILayout.Label(Path.GetFileName(playlistFiles[i]), labelStyle, buttonLayoutOptions);
-                    if (GUILayout.Button("Remove", buttonStyle, buttonLayoutOptions))
+                    GUILayout.Label(Path.GetFileName(playlistFiles[i]));
+                    if (GUILayout.Button("Remove"))
                     {
                         playlistFiles.RemoveAt(i);
                         SavePlaylists();
@@ -387,13 +302,13 @@ namespace MusicPlayer
                     GUILayout.EndHorizontal();
                 }
 
-                GUILayout.BeginHorizontal(paginationLayoutOptions);
+                GUILayout.BeginHorizontal();
                 GUILayout.FlexibleSpace();
-                if (playlistPage > 0 && GUILayout.Button("Previous Page", buttonStyle, buttonLayoutOptions))
+                if (playlistPage > 0 && GUILayout.Button("Previous Page"))
                 {
                     playlistPage--;
                 }
-                if (endIndex < playlistFiles.Count && GUILayout.Button("Next Page", buttonStyle, buttonLayoutOptions))
+                if (endIndex < playlistFiles.Count && GUILayout.Button("Next Page"))
                 {
                     playlistPage++;
                 }
@@ -402,7 +317,7 @@ namespace MusicPlayer
 
                 GUILayout.Space(10);
 
-                if (GUILayout.Button("Shuffle", buttonStyle))
+                if (GUILayout.Button("Shuffle"))
                 {
                     shuffle = !shuffle;
                 }
@@ -494,18 +409,6 @@ namespace MusicPlayer
             }
         }
 
-        private static Texture2D MakeTex(int width, int height, Color col)
-        {
-            Color[] array = new Color[width * height];
-            for (int i = 0; i < array.Length; i++)
-            {
-                array[i] = col;
-            }
-            Texture2D texture2D = new Texture2D(width, height);
-            texture2D.SetPixels(array);
-            texture2D.Apply();
-            return texture2D;
-        }
 
         private static string YesNo(bool input)
         {
